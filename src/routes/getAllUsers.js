2d571acc6872216923413f92e68c65e553c6903c
willem <willem@dsl-50-5-182-28.fuse.net>
2022-03-24 15:31:50 -0400

    added some async code

--- src/routes/getAllUsers.js
+++ src/routes/getAllUsers.js
@@ -1,9 +1,11 @@
-const { getUsers } = require('../core/users')
+const { getUsers, getUsersAsync } = require('../core/users')
 
 module.exports = async (req , res) => {
 
-    getUsers( data => {
-        res.status(200).send({message: data})
-    })
-    
+    // getUsers( data => {
+    //     res.status(200).send({message: data})
+    // })
+    const users = await getUsersAsync()
+    console.log(users)
+    res.status(200).send({message: users})
 }
